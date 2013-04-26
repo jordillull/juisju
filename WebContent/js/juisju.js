cc0ec918f76b4947e82e303a5262e285330323ba
Jordi Llull <jllull@habitissimo.com>
2013-04-26 17:51:58 +0200

    well done

---
+++ WebContent/js/juisju.js
@@ -0,0 +1,521 @@
+var data = { "people":
+    [
+     {
+      "name": "Eduardo Sarciat Girón",
+      "birthdate"  : "1985-08-08",
+      "startdate"  : "2011-10-22",
+      "country"    : "España",
+      "food"       : "Carne roja. Mucha.",
+      "movie"      : "Tiempos de gloria",
+      "superpower" : "Volar siendo invisible",
+      "photo"      : "http://26.media.tumblr.com/tumblr_liac42T0pN1qzj3syo1_400.jpg"
+    },{
+      "name"       : "Adrián Cánovas Rosales",
+      "birthdate"  : "1991-06-26",
+      "startdate"  : "2013-03-11",
+      "country"    : "España",
+      "food"       : "Carne de buey a la piedra",
+      "movie"      : "Origen",
+      "superpower" : "Viajar en el tiempo",
+      "photo"      : "http://24.media.tumblr.com/tumblr_lsvcpxVBgd1qzgqodo1_500.jpg"
+    },{
+      "name"       : "Ismael Sitnes Rodríguez",
+      "birthdate"  : "1990-01-19",
+      "startdate"  : "2012-11-26",
+      "country"    : "España",
+      "food"       : "Pasta carbonara",
+      "movie"      : "Pearl Harbour",
+      "superpower" : "Teletransporte",
+      "photo"      : "http://25.media.tumblr.com/tumblr_litk67P87K1qaa50yo1_500.jpg"
+    },{
+      "name"       : "Verónica Rosselló",
+      "birthdate"  : "1978-02-20",
+      "startdate"  : "2013-04-08",
+      "country"    : "Argentina",
+      "food"       : "Ensalada",
+      "movie"      : "La triología de Bourne",
+      "superpower" : "Adivinar que piensan los periodistas",
+      "photo"      : "http://28.media.tumblr.com/tumblr_lk82r5fJzw1qebvsbo1_500.jpg"
+    },{
+      "name"       : "Aina Nadal Cárdenas",
+      "birthdate"  : "1988-09-14",
+      "startdate"  : "2012-09-03",
+      "country"    : "España",
+      "food"       : "Pasta rellena",
+      "movie"      : "En busca de la felicidad",
+      "superpower" : "Cambio de color y forma corporal",
+      "photo"      : "http://28.media.tumblr.com/tumblr_lja6xpfr4X1qaa50yo1_500.jpg"
+    },{
+      "name"       : "Jordi Llull Chavarría",
+      "birthdate"  : "1986-11-01",
+      "startdate"  : "2013-03-04",
+      "country"    : "España",
+      "food"       : "Sushi",
+      "movie"      : "Delhi Belly",
+      "superpower" : "Parar el tiempo",
+      "photo"      : "http://25.media.tumblr.com/tumblr_mcei0s9zPy1qzxd5lo1_500.jpg"
+    },{
+      "name"       : "Martin Caleau",
+      "birthdate"  : "1981-02-15",
+      "startdate"  : "Año 0 A.C. (Enero de 2009)",
+      "country"    : "Argentina",
+      "food"       : "Milanesas",
+      "movie"      : "Star Wars",
+      "superpower" : "Manipulación del tiempo",
+      "photo"      : "http://27.media.tumblr.com/tumblr_liiy8xlcFx1qbeocxo1_500.jpg"
+    },{
+      "name"       : "Jordi Ber",
+      "birthdate"  : "1976-07-04",
+      "startdate"  : "2009",
+      "country"    : "2009-01-01",
+      "food"       : "Pa amb oli",
+      "movie"      : "Wall-E",
+      "superpower" : "Volar",
+      "photo"      : "http://25.media.tumblr.com/tumblr_ljvqe7tzo21qa9raco1_500.jpg"
+    },{
+      "name"       : "Maria Luján",
+      "birthdate"  : "1975-11-02",
+      "startdate"  : "2010-12-02",
+      "country"    : "Argentina",
+      "food"       : "Milanesa napolitana",
+      "movie"      : "El secreto de tus ojos",
+      "superpower" : "Teletransporte",
+      "photo"      : "http://25.media.tumblr.com/tumblr_mblaw5jZ0v1qbbpjfo1_500.jpg"
+    },{
+      "name"       : "Leidy Tatiana Cuartas Vélez",
+      "birthdate"  : "1987-07-06",
+      "startdate"  : "2012-12-10",
+      "country"    : "Colombia",
+      "food"       : "Alubias",
+      "movie"      : "Fin del mundo 2012",
+      "superpower" : "Ninguno",
+      "photo"      : "http://28.media.tumblr.com/tumblr_ljguyvQfz81qzy9e6o1_500.jpg"
+    },{
+      "name"       : "Manuel Antonio Álvarez Alasen",
+      "birthdate"  : "1990-09-01",
+      "startdate"  : "2012-04-10",
+      "country"    : "Suiza",
+      "food"       : "Tacos",
+      "movie"      : "Las tortugas también vuelan",
+      "superpower" : "Controlar el tiempo",
+      "photo"      : "http://24.media.tumblr.com/tumblr_lhv54jZCns1qzj3syo1_500.jpg"
+    },{
+      "name"       : "Gustavo Silva",
+      "birthdate"  : "1969-05-24",
+      "startdate"  : "2013-03-04",
+      "country"    : "Argentina",
+      "food"       : "Sushi",
+      "movie"      : "El Padrino",
+      "superpower" : "Fuerza biónica",
+      "photo"      : "http://30.media.tumblr.com/tumblr_lisw9j4LZM1qbbpjfo1_400.jpg"
+    },{
+      "name"       : "Jose Manuel",
+      "birthdate"  : "1998-03-21",
+      "startdate"  : "2013-03-07",
+      "country"    : "España",
+      "food"       : "Canelones",
+      "movie"      : "El Origen",
+      "superpower" : "Super fuerza",
+      "photo"      : "http://25.media.tumblr.com/tumblr_lm1gf8IEiu1qko977o1_500.jpg"
+    },{
+      "name"       : "Alberto Costa Algaba",
+      "birthdate"  : "1990-10-15",
+      "startdate"  : "2012-10-01",
+      "country"    : "España",
+      "food"       : "Tallarines Carbonara",
+      "movie"      : "Origen",
+      "superpower" : "Controlar el tiempo",
+      "photo"      : "http://25.media.tumblr.com/tumblr_maxm4ecVNq1qjmqavo1_500.jpg"
+    },{
+      "name"       : "Silvia Battistella",
+      "birthdate"  : "1974-09-06",
+      "startdate"  : "2103-04-16",
+      "country"    : "Italia",
+      "food"       : "Lasaña",
+      "movie"      : "Django unchained",
+      "superpower" : "Predicción del futuro",
+      "photo"      : "http://24.media.tumblr.com/tumblr_liivjs579l1qeqteyo1_500.jpg"
+    },{
+      "name"       : "Germán March",
+      "birthdate"  : "1987-04-16",
+      "startdate"  : "2012-01-01",
+      "country"    : "España",
+      "food"       : "Frit mallorquí",
+      "movie"      : "Quadrophenia",
+      "superpower" : "Curativo",
+      "photo"      : "http://26.media.tumblr.com/tumblr_liac42T0pN1qzj3syo1_400.jpg"
+    },{
+      "name"       : "Juan Contestí",
+      "birthdate"  : "1978-12-24",
+      "startdate"  : "2011-10-01",
+      "country"    : "España",
+      "food"       : "Japonesa",
+      "movie"      : "Intocable",
+      "superpower" : "La fuerza (Jedi)",
+      "photo"      : "http://24.media.tumblr.com/tumblr_lima5j6wR11qzqe35o1_500.jpg"
+    },{
+      "name"       : "Sara Ambrogi",
+      "birthdate"  : "1989-06-09",
+      "startdate"  : "2013-02-19",
+      "country"    : "Filipinas",
+      "food"       : "Arroz a la cubana",
+      "movie"      : "La cena de los idiotas (FR)",
+      "superpower" : "Detener el tiempo",
+      "photo"      : "http://26.media.tumblr.com/tumblr_ll90kwmMJw1qzj3syo1_500.jpg"
+    },{
+      "name"       : "Leandro Barreiro",
+      "birthdate"  : "1978-06-21",
+      "startdate"  : "2013-01-14",
+      "country"    : "Brasil",
+      "food"       : "Pizza",
+      "movie"      : "En busca de la felicidad",
+      "superpower" : "Volar",
+      "photo"      : "http://28.media.tumblr.com/tumblr_ljczkiDmrM1qe3m7qo1_400.jpg"
+    },{
+      "name"       : "Paolo Esposito",
+      "birthdate"  : "1988-02-01",
+      "startdate"  : "2012-04-02",
+      "country"    : "Italia",
+      "food"       : "Lechona",
+      "movie"      : "La vita è bella",
+      "superpower" : "No necesito superpoderes",
+      "photo"      : "http://27.media.tumblr.com/tumblr_ll3xua50Vr1qb08qmo1_500.jpg"
+    },{
+      "name"       : "Viola Valdo",
+      "birthdate"  : "1980-11-22",
+      "startdate"  : "2013-02-04",
+      "country"    : "Italia",
+      "food"       : "Pasta",
+      "movie"      : "Lincoln",
+      "superpower" : "Invisibilidad",
+      "photo"      : "http://26.media.tumblr.com/tumblr_ljju0frmo81qaa50yo1_400.jpg"
+    },{
+      "name"       : "Rafa Martos",
+      "birthdate"  : "1986-04-28",
+      "startdate"  : "2012-12-10",
+      "country"    : "España",
+      "food"       : "Chuletón con patatas",
+      "movie"      : "Planet Terror",
+      "superpower" : "Esqueleto de adamantita y regeneración (Lobezno)",
+      "photo"      : "http://30.media.tumblr.com/tumblr_lj50gs8rAX1qaa50yo1_500.jpg"
+    },{
+      "name"       : "Adriana Cristofolini",
+      "birthdate"  : "1973-10-29",
+      "startdate"  : "2012-04-16",
+      "country"    : "Brasil",
+      "food"       : "Feijoada",
+      "movie"      : "Cidade de Deus",
+      "superpower" : "Viajar en el tiempo",
+      "photo"      : "http://25.media.tumblr.com/tumblr_ll3y1pZDVJ1qb08qmo1_500.jpg"
+    },{
+      "name"       : "Estefanía Alvarez",
+      "birthdate"  : "1986-11-03",
+      "startdate"  : "2012-06-01",
+      "country"    : "Argentina",
+      "food"       : "Milanesa con patatas fritas",
+      "movie"      : "El secreto de sus ojos",
+      "superpower" : "Teletransportación",
+      "photo"      : "http://29.media.tumblr.com/tumblr_ll3xdp73DQ1qb08qmo1_500.jpg"
+    },{
+      "name"       : "Patricia Mulet",
+      "birthdate"  : "1977-10-21",
+      "startdate"  : "2011-03-01",
+      "country"    : "España",
+      "food"       : "Patatas fritas",
+      "movie"      : "Pelis no! Libros sí!",
+      "superpower" : "Leer la mente",
+      "photo"      : "http://27.media.tumblr.com/tumblr_lit0fgki1Z1qfh1tao1_500.jpg"
+    },{
+      "name"       : "Viola Valdo",
+      "birthdate"  : "1980-11-22",
+      "startdate"  : "2013-02-04",
+      "country"    : "Italia",
+      "food"       : "Pasta",
+      "movie"      : "Lincoln",
+      "superpower" : "Invisibilidad",
+      "photo"      : "http://28.media.tumblr.com/tumblr_lk0n7cSLof1qdvswbo1_500.png",
+    },{
+      "name"       : "Silvia Arcadi",
+      "birthdate"  : "1977-02-04",
+      "startdate"  : "2011-12-01",
+      "country"    : "Italia",
+      "food"       : "Udones japoneses",
+      "movie"      : "Pulp Fiction",
+      "superpower" : "Volar",
+      "photo"      : "http://28.media.tumblr.com/tumblr_llayqvxQOG1qaa50yo1_500.jpg"
+    },{
+      "name"       : "Semra Halilova",
+      "birthdate"  : "1979-01-21",
+      "startdate"  : "2013-04-08",
+      "country"    : "Bulgaria",
+      "food"       : "Todo tipo de pasta",
+      "movie"      : "Gladiator",
+      "superpower" : "Una varita mágica",
+      "photo"      : "http://27.media.tumblr.com/tumblr_lteewoQF9A1qb08qmo1_500.jpg"
+    },{
+      "name"       : "Verónica Nese Decaro",
+      "birthdate"  : "1983-10-03",
+      "startdate"  : "2012-11-26",
+      "country"    : "Uruguay",
+      "food"       : "Sushi",
+      "movie"      : "Django unchained",
+      "superpower" : "Volar",
+      "photo"      : "http://24.media.tumblr.com/tumblr_lhwsxaMJfm1qaa50yo1_500.jpg"
+    },{
+      "name"       : "Melisa Adriana Díaz Nunes",
+      "birthdate"  : "1981-03-09",
+      "startdate"  : "2013-02-18",
+      "country"    : "Argentina",
+      "food"       : "Entraña MMMM...",
+      "movie"      : "Taxidermia",
+      "superpower" : "Teletransporte al medievo",
+      "photo"      : "http://24.media.tumblr.com/tumblr_ll1h2iI9pG1qe76kxo1_500.jpg"
+    },{
+      "name"       : "Deborah Rojas",
+      "birthdate"  : "1974-09-03",
+      "startdate"  : "2012-11-26",
+      "country"    : "Chile",
+      "food"       : "Mexicana",
+      "movie"      : "Titanic",
+      "superpower" : "Invisibilidad",
+      "photo"      : "http://30.media.tumblr.com/tumblr_lqmbfnCsMx1qfji2jo1_500.jpg"
+    },{
+      "name"       : "Marcelo Luiz do Carmo",
+      "birthdate"  : "1982-03-01",
+      "startdate"  : "2013-03-19",
+      "country"    : "Brasil",
+      "food"       : "Judías negras",
+      "movie"      : "Radio Favela",
+      "superpower" : "Super fuerza",
+      "photo"      : "http://25.media.tumblr.com/tumblr_mavsumGGAd1qboaw8o1_500.jpg"
+    },{
+      "name"       : "Ana Paula Viana",
+      "birthdate"  : "1982-11-21",
+      "startdate"  : "2013-02-04",
+      "country"    : "Brasil",
+      "food"       : "Japonesa / Sushi",
+      "movie"      : "El Club de la Lucha",
+      "superpower" : "Leer la mente de la gente, volar y ser invisible!",
+      "photo"      : "http://25.media.tumblr.com/tumblr_lttbk92Ko01ql9nqgo1_500.jpg"
+    },{
+      "name"       : "Cleber Cerqueixa",
+      "birthdate"  : "1980-06-10",
+      "startdate"  : "2012-09-03",
+      "country"    : "Brasil",
+      "food"       : "Feijoada",
+      "movie"      : "Cidade de Deus",
+      "superpower" : "Volar",
+      "photo"      : "http://30.media.tumblr.com/tumblr_lqgsins4RV1qd97mjo1_500.jpg"
+    },{
+      "name"       : "Jaume Alemany",
+      "birthdate"  : "1980-06-08",
+      "startdate"  : "2012-06-13",
+      "country"    : "España",
+      "food"       : "Croquetas",
+      "movie"      : "Pulp Fiction",
+      "superpower" : "Invisibilidad",
+      "photo"      : "http://28.media.tumblr.com/tumblr_lk5h7hIRFf1qi4pifo1_500.jpg"
+    },{
+      "name"       : "Eva Gómez",
+      "birthdate"  : "1973-08-11",
+      "startdate"  : "2013-04-02",
+      "country"    : "España",
+      "food"       : "Japonesa",
+      "movie"      : "Criadas y Señoras",
+      "superpower" : "Telepatía",
+      "photo"      : "http://25.media.tumblr.com/tumblr_mbz2iqOhmU1qb08qmo1_400.gif",
+    },{
+      "name"       : "Marta Mayol",
+      "birthdate"  : "1971-03-08",
+      "startdate"  : "2010-08-01",
+      "country"    : "España",
+      "food"       : "Chocolate",
+      "movie"      : "Atrapado en el tiempo",
+      "superpower" : "Telequinesia",
+      "photo"      : "http://24.media.tumblr.com/tumblr_lsm50jkqpy1qzhmgco1_500.jpg",
+    },{
+      "name"       : "Elisa Nieto",
+      "birthdate"  : "1976-09-08",
+      "startdate"  : "2010-09-01",
+      "country"    : "España",
+      "food"       : "Hindú",
+      "movie"      : "Orgullo y Prejuicio",
+      "superpower" : "Volar (y no tener vértigo)",
+      "photo"      : "http://25.media.tumblr.com/tumblr_ls9fa8S9lE1qfbmwho1_500.jpg",
+    },{
+      "name"       : "Julieta Mora",
+      "birthdate"  : "1981-11-13",
+      "startdate"  : "2012-01-30",
+      "country"    : "Argentina",
+      "food"       : "Feijoada",
+      "movie"      : "Midnight in Paris",
+      "superpower" : "Volar! O teletransportarme",
+      "photo"      : "http://27.media.tumblr.com/tumblr_lhty7gGku61qb08qmo1_500.jpg",
+    },{
+      "name"       : "Hugo Chinchilla",
+      "birthdate"  : "1988-08-25",
+      "startdate"  : "2010-04-01",
+      "country"    : "España",
+      "food"       : "Crema de espinacas",
+      "movie"      : "Iron Man 3",
+      "superpower" : "root",
+      "photo"      : "http://29.media.tumblr.com/tumblr_lhv84py3Ff1qzj3syo1_500.jpg",
+    }
+//    {
+//      "name"       : "",
+//      "birthdate"  : "",
+//      "startdate"  : "",
+//      "country"    : "",
+//      "food"       : "",
+//      "movie"      : "",
+//      "superpower" : "",
+//      "photo"      : "",
+//    }
+  ],
+  "errors": ["Nop", "Ups", "Prueba otra vez", "Va a ser que no", "Uy...", "Pues...no", "Casi", "Tampoco", "Buen intento"],
+  "success": ["Muy bien", "Genial!", "Fantástico!", "Deslumbrante", "Apoteósico", "Acertaste!"]
+};
+
+Function.prototype.scope = function(scope) {
+    var fn = this;
+    return function() {
+        // Forward to the original function using 'scope' as 'this'.
+        return fn.apply(scope, arguments);
+    };
+};
+
+String.prototype.removeAccents = function ()
+{
+  var __r = 
+  {
+    'À':'A','Á':'A','Â':'A','Ã':'A','Ä':'A','Å':'A','Æ':'E',
+    'È':'E','É':'E','Ê':'E','Ë':'E',
+    'Ì':'I','Í':'I','Î':'I',
+    'Ò':'O','Ó':'O','Ô':'O','Ö':'O',
+    'Ù':'U','Ú':'U','Û':'U','Ü':'U',
+    'Ñ':'N'
+  };
+  
+  return this.replace(/[ÀÁÂÃÄÅÆÈÉÊËÌÍÎÒÓÔÖÙÚÛÜÑ]/gi, function(m)
+  {
+    var ret = __r[m.toUpperCase()];
+
+    if (m === m.toLowerCase())
+      ret = ret.toLowerCase();
+
+    return ret;
+  });
+}; 
+
+var random_error_msg = function() { 
+  return data.errors[Math.floor(Math.random()*data.errors.length)];
+};
+
+var random_success_msg = function() {
+  return data.success[Math.floor(Math.random()*data.errors.length)];
+};
+
+ 
+var People = Class.extend({
+  init: function() {
+    this.pointer = 0;
+    this.people = data.people;
+    this.next();
+  },
+  next: function() {
+    this.pointer = Math.floor(Math.random()*data.people.length);
+  },
+  get: function() {
+    return data.people[this.pointer];
+  },
+  get_info: function() {
+    person = data.people[this.pointer]; 
+    return '<dl class="dl-horizontal">' 
+            +"<dt>Nombre</dt>"+ "<dd>"+person.name+"</dd>"
+            +"<dt>Edad</dt>"  + "<dd>"+person.birthdate+"</dd>"
+            +"<dt>País</dt>"  + "<dd>"+person.country+"</dd>"
+            +"<dt>En habitissimo desde</dt>"  + "<dd>"+person.startdate+"</dd>"
+            +"<dt>Comida favorita</dt>"  + "<dd>"+person.food+"</dd>"
+            +"<dt>Una película</dt>"  + "<dd>"+person.movie+"</dd>"
+            +"<dt>Superpoder</dt>"  + "<dd>"+person.superpower+"</dd>"
+          +"</dl>"
+          ;
+  },
+});
+
+var check_name = function (input, name) {
+  guess = input.removeAccents();
+  if (guess == "edu") {
+    guess = "eduardo";
+  }
+  if (guess == "eva") {
+    guess = "eva "
+  }
+  if (guess == "ana") {
+    guess = "ana ";
+  }
+  if (guess.length > 3) {
+    if (name.toLowerCase().removeAccents().match(guess.toLowerCase())) {
+      return true;
+    } else{
+      return false;
+    }              
+  }
+};
+
+var Guesser = Class.extend({
+  init: function(element)  {
+    this.element = element;    
+    this.next_btn = element.find('#next');
+    this.surrender_btn = element.find('#surrender');
+    this.surrender_btn = element.find('#surrender');
+    this.photo = element.find('#photo');
+    this.answer = element.find('#answer');
+    this.input = element.find('input'); 
+    this.image = element.find('#main_photo'); 
+    this.last_image = element.find('#last_photo'); 
+    this.last_info = element.find('#last_info'); 
+    this.people = new People();
+    
+    this.next_btn.bind('click', this.guess.scope(this));
+    this.input.bind('keyup', this.keyup.scope(this));
+    this.surrender_btn.bind('click', this.surrender.scope(this));
+    
+    this.refresh();
+  },
+  keyup: function(event) {
+    if (event.which == '13') {
+      this.next_btn.triggerHandler('click');
+    }
+  },
+  surrender: function() {
+    this.answer.html('Este careto es de '+this.people.get().name);
+    this.answer.show(); 
+  },
+  guess: function() { 
+    if (check_name(this.input.val(), this.people.get().name)) {
+      this.answer.show();      
+      this.answer.html(random_success_msg());
+      this.next();
+    } 
+    else {
+      this.answer.show();      
+      this.answer.html(random_error_msg());
+    }
+  },
+  photo: function(name) {
+    return "holder.js/200x300";
+  },
+  next: function() { 
+    this.last_image.attr('src', this.image.attr('src'));
+    this.last_info.html(this.people.get_info());
+    this.last_info.html(this.people.get_info());
+    this.people.next();
+    this.refresh();
+  },
+  refresh: function() {
+    this.input.val('');
+    this.image.attr('src', this.people.get().photo);
+    this.answer.hide();
+  }
+});
---
+++ WebContent/people.php
@@ -0,0 +1,19 @@
+<?php
+
+$people = [
+  (object) [
+    'name'  => 'Hugo Chinchilla',
+    'role'  => 'Developer',
+    'q1'    => 'Crema de espinacas',
+    'q2'    => '',
+    'q3'    => 'root privileges',
+    'img'   => 'holder.js/120x120'],
+
+  (object) [
+    'name'  => 'Hugo Chinchilla',
+    'role'  => 'Developer',
+    'q1'    => 'Crema de espinacas',
+    'q2'    => '',
+    'q3'    => 'Michelangelo',
+    'img'   => 'holder.js/120x120'],
+];
